60d98c6881ed70388f73f439566977f7f148a539
Cody_Gua <cody23333@gmail.com>
2022-04-09 22:41:30 +0800

    cmake能成功编译，但是win下程序运行不正常

--- cv/src/main.cpp
+++ cv/src/main.cpp
@@ -7,16 +7,16 @@
  * 
  */
 
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
 int main() {
 
     cv::Mat img = cv::imread("wind-turbine.jpg", cv::IMREAD_COLOR);
-    cv::putText(img, "Test text", cv::Point(100, 100), cv::FONT_ITALIC, 2.0,
-                cv::Scalar(0, 0, 255), 2);
+    cv::putText(img, "Test text", cv::Point(100, 100), cv::FONT_ITALIC, 2.0, cv::Scalar(0, 0, 255), 2);
     cv::imwrite("text_img.jpg", img);
+    cout<<"ok"<<endl;
 
     return 0;
 }
