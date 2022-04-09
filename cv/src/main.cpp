bff93ebb57ee4246dbfd8952c8b08758b98a76e8
discodyer <cody23333@gmail.com>
2022-04-09 14:02:51 +0800

    添加注释

--- cv/src/main.cpp
+++ cv/src/main.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file main.cpp
+ * @author CodyGua (cody23333@gmail.com)
+ * @brief 将一个图片上添加测试文字
+ * @version 0.1
+ * @date 2022-04-09
+ * 
+ */
+
 #include "opencv2/opencv.hpp"
 #include <iostream>
 using namespace std;
--- cv/src/test.cpp
+++ cv/src/test.cpp
@@ -1,8 +1,11 @@
-//utf-8
-//文件名称 test.cpp
-//创建日期 3/30/22
-//最后一次编辑 3/30/22 by cody
-//简介 最简单的C++程序，用于检测编译环境是否正常
+/** utf-8
+ * @file test.cpp
+ * @author CodyGua (cody23333@gmail.com)
+ * @brief 最简单的C++程序，用于检测编译环境是否正常
+ * @version 0.1
+ * @date 2022-03-22
+ * 
+ */
 
 #include<iostream>
 using namespace std;
