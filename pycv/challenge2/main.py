14ee7bdf0cfd864f32c6e0f1ede2d294339e3b7e
codygua <cody23333@gmail.com>
2022-04-17 17:52:55 +0800

    pycharm 自动修正语法

--- pycv/challenge2/main.py
+++ pycv/challenge2/main.py
@@ -1,8 +1,8 @@
 from src.shapedectct import Picture
 
-#实例化一个新的对象 需要输入文件路径
+# 实例化一个新的对象 需要输入文件路径
 test = Picture("test.jpg")
-#放一个文字，需要输入一个string
+# 放一个文字，需要输入一个string
 test.putText("wwwwwwww")
-#展示图片
+# 展示图片
 test.show()
--- pycv/challenge2/src/shapedectct/Picture.py
+++ pycv/challenge2/src/shapedectct/Picture.py
@@ -1,12 +1,13 @@
-import cv2 
+import cv2
 from cv2 import imread
 
-class Picture:                                                      #创建 Picture 类
 
-    def __init__(self, path_to_img) -> None:                        #初始化函数，要实例化一个新的对象需要输入文件路径
+class Picture:  # 创建 Picture 类
+
+    def __init__(self, path_to_img) -> None:  # 初始化函数，要实例化一个新的对象需要输入文件路径
         self.img = imread(path_to_img, cv2.IMREAD_COLOR)
 
-    def putText(self, string_to_write):                             #实现 放一个文字，需要输入一个string
+    def putText(self, string_to_write):  # 实现 放一个文字，需要输入一个string
         org = (100, 100)
         fontFace = cv2.FONT_HERSHEY_SIMPLEX
         fontScale = 1
@@ -17,10 +18,8 @@ class Picture:                                                      #创建 Pict
         # cv2.imshow('image', img)
         # cv2.waitKey(0) 
         # cv2.destroyAllWindows() 
-        
-    def show(self):                                                 #实现 展示图片
+
+    def show(self):  # 实现 展示图片
         cv2.imshow("image", self.img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-
